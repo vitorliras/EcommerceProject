5df87e07eb731a0afec1546b3ec23b9035f1e007
vitorliras <vitorlirasti@hotmail.com>
2023-01-19 09:28:18 -0300

    Excluindo os arquivos com nomes errados

--- Client/Services/CategoryService/ICategoryServicecs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EcommerceProject.Client.Services.CategoryService
-{
-    public interface ICategoryService
-    {
-        List<Category> Categories { get; set; }
-        Task GetCategories();
-        //Task<ServiceResponse<Product>> GetProduct(int productId);
-    }
-}
